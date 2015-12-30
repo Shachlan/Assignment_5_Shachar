f6f61fd235f58e5a7a1de431384968778abce1c9
Shachar Langbeheim <shachar@lightricks.com>
2015-12-30 09:53:54 +0200

    FlickrLocationParser : initial commit.
    This commit adds FlickrLocationParser, an immutable object creating LocationInfo objects from dictionaries in the Flickr photo metadata format.

---
+++ Exercise5/Flickr/FlickrLocationParser.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2015 Lightricks. All rights reserved.
+// Created by Shachar Langbeheim.
+
+#import "LocationParser.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// Immutable object creating \c LocationInfo objects from dictionaries/// in the Flickr photo
+/// metadata format.
+@interface FlickrLocationParser : NSObject <LocationParser>
+@end
+
+NS_ASSUME_NONNULL_END
